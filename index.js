63ac09fc5f9d2872ed152ccd686622d4ecb19849
imshota <imshotaseo@icloud.com>
2020-11-21 21:23:33 +0900

    add modules files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+// GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
