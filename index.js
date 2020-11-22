e9977da1678964f8eaa425e9c0e77b9d49dd84c3
imshota <imshotaseo@icloud.com>
2020-11-22 10:21:15 +0900

    hash

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const crypto = require('crypto')
+var fs = require('fs');
 const port = 3000
 
 app.use(bodyParser.urlencoded({
@@ -37,8 +39,26 @@ app.post('/', function (req, res) {
   const List = { "status": "your message has been received" };
   console.log(`POST is sended`)
   console.log(req.body);
+  var sha512 = crypto.createHash('sha512');
+  sha512.update(req.body.comment)
+  var hash = sha512.digest('hex')
+  console.log(hash);
+
+  fs.readFile('encoded.png', 'utf8', function (err, data) {
+    var decode = new Buffer(data, 'base64');
+    fs.writeFile('xxx.png', decode, function (err) {
+      console.log(err);
+    });
+  });
 
   // POST accion
+  /*  
+  const data = {
+    type: this.checked,
+    btName: this.btName,
+    comment: this.comment
+  }
+  */
   res.json(List);
 })
 
